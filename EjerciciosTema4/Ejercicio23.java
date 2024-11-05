56328832c73e8b05694a7a10a3c87244d1ddd932
DDDBBBPPP <david.bellon.payer@gmail.com>
2024-11-05 17:03:19 +0100

    ejercicio 23

--- EjerciciosTema4/Ejercicio23.java
+++ EjerciciosTema4/Ejercicio23.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 
 public class Ejercicio23 {
     public static void main(String[] args) {
-        int altura;
-        int anchura;
+        float altura;
+        float anchura;
         float calculo;
+        float escudo = 2.5f;
         String bordado;
         
         Scanner sc = new Scanner(System.in).useLocale(Locale.ENGLISH);
@@ -15,13 +16,25 @@ public class Ejercicio23 {
         altura = sc.nextFloat();
         System.out.print("Ahora introduzca la anchura: ");
         anchura = sc.nextFloat();
+        sc.nextLine();
         System.out.print("¿Quiere escudo bordado? (s/n): ");
         bordado = sc.nextLine();
-        if(altura <=0 || anchura<=0 || bordado != "s" || bordado != "n"){
+        if(altura <=0 || anchura<=0 || !bordado.equals("s") && !bordado.equals("n")){
             System.out.println("Introduce datos válidos.");
         }else{
-            calculo
-            System.out.printf("Bandera de %d cm cuadrados: %.2f $\n", altura*anchura,  );
+
+            calculo = altura*anchura/100;
+            System.out.printf("Bandera de %.0f cm cuadrados: %.2f $\n", altura*anchura, calculo );
+            if(bordado.equals("s")){
+                calculo = calculo + escudo + 3.25f;
+                System.out.printf("Con escudo: %.2f $\n", escudo);
+            }else{
+                calculo = calculo + 3.25f;
+                System.out.printf("Sin escudo: %.2f $\n", 0.0f);
+            }
+            System.out.printf("Gastos de envío: %.2f $\n", 3.25);
+            System.out.printf("Total: %.2f\n",calculo );
+
 
         }
         sc.close();
