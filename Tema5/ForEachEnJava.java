d3be6e8d4e739685abdc617af2346801e5c9bcd3
DDDBBBPPP <david.bellon.payer@gmail.com>
2024-12-02 14:44:10 +0100

    YAA

--- Tema5/ForEachEnJava.java
+++ Tema5/ForEachEnJava.java
@@ -4,16 +4,25 @@ public class ForEachEnJava {
     public static void main(String[] args) {
 
         int[] numeros = new int[31];
+        int[][] bidimensional = new int[12][31];
 
-        //Esto recorre todo el array.
-        for(int item: numeros){
+        // Esto recorre todo el array unidimensional.
+        for (int item : numeros) {
             System.out.print(item + " ");
 
         }
 
-        /* // Si pones foreach te hace esto del tirón
-        for (int i : numeros) {
-            
-        } */
+        /*
+         * // Si pones foreach te hace esto del tirón
+         * for (int i : numeros) {
+         * 
+         * }
+         */
+        // Para recorrer bidimensionales
+        for (int[] fila : bidimensional) {
+            for (int item : fila) {
+                System.out.printf("%d ", item);
+            }
+        }
     }
 }
