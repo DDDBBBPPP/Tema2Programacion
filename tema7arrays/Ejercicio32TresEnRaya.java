3c1c3c1f720fe0670580c3e10b344cacae5d97fa
DDDBBBPPP <david.bellon.payer@gmail.com>
2025-01-08 20:00:04 +0100

    3 en raya done

--- tema7arrays/Ejercicio32TresEnRaya.java
+++ tema7arrays/Ejercicio32TresEnRaya.java
@@ -7,6 +7,7 @@ public class Ejercicio32TresEnRaya {
         int j;
         int filEle = ' ';
         int colEle;
+        int contador = 0;
         String elec;
         String textoFinal = "";
         boolean finito = false;
@@ -18,12 +19,14 @@ public class Ejercicio32TresEnRaya {
         }
 
         do{
+            if(contador%2==0){
 
-            maquina(matriz);
-            Thread.sleep(1000);
-            pintar(matriz);
-            textoFinal
-            
+                maquina(matriz);
+                Thread.sleep(1000);
+                pintar(matriz);
+            }else{
+
+                
             //Se elige la posicion y se transforma para el array.
             System.out.print("Introduzca las coordenadas, por ejemplo b2: ");
             elec = System.console().readLine();
@@ -42,76 +45,139 @@ public class Ejercicio32TresEnRaya {
         matriz[filEle][colEle] = 'X';
         pintar(matriz);
         
+
+
+            }
+            contador++;
+            textoFinal = mensaje(matriz);
+            if(textoFinal.equals("")){
+
+            }else{
+                System.out.println(textoFinal);
+                finito = true;
+            }
+               
     }while(!finito);
     }
 
-
-
-     /**
-      * Pinta la matriz
-      * @param matriz
-      */
-    public static void pintar(char[][] matriz){
+    /**
+     * Pinta la matriz
+     * 
+     * @param matriz
+     */
+    public static void pintar(char[][] matriz) {
         System.out.println("  -------");
-        for(int i = 0; i<matriz.length;i++){
-            if(i==0){System.out.print("a ");}
-            if(i==1){System.out.print("b ");}
-            if(i==2){System.out.print("c ");}
-            
-            for(int j = 0; j<matriz[i].length;j++){
-             System.out.print("|" + matriz[i][j]);
+        for (int i = 0; i < matriz.length; i++) {
+            if (i == 0) {
+                System.out.print("a ");
+            }
+            if (i == 1) {
+                System.out.print("b ");
+            }
+            if (i == 2) {
+                System.out.print("c ");
+            }
+
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print("|" + matriz[i][j]);
             }
             System.out.print("|");
             System.out.println();
-        }       
+        }
         System.out.println("  -------");
 
         System.out.println("   1 2 3");
     }
 
-
     /**
      * la maquina juega.
+     * 
      * @param matriz
      */
-    public static void maquina(char[][] matriz){
+    public static void maquina(char[][] matriz) {
         int j;
         int i;
         boolean hecho = false;
-        do{
-            i = (int)(Math.random()*3);
-            j = (int)(Math.random()*3);
+        do {
+            i = (int) (Math.random() * 3);
+            j = (int) (Math.random() * 3);
 
-            if(matriz[i][j]==' '){
+            if (matriz[i][j] == ' ') {
                 matriz[i][j] = '0';
                 hecho = true;
             }
-        }while(!hecho);
+        } while (!hecho);
     }
 
-    public static String mensaje(char[][] matriz){
-        char c;
-        String msj =  "";
+    public static String mensaje(char[][] matriz) {
+        String msj = "";
         int contador = 0;
-        int contFila;
+        int contFilaMaq = 0;
+        int contFilaJug = 0;
+        int c;
 
-        
-        for(int i = 0;i<matriz.length;i++){
-        
-            for(int j = 0;j<matriz[i].length;j++){
-                c = matriz[i][j];
-                //Esto para empate.
-                if(matriz[i][j]==' '){
+        // Las diagonales lo primero
+        if (matriz[0][0]=='0' && matriz[1][1]=='0' && matriz[2][2]=='0'){
+            msj = "¡Has Perdido!";
+        }
+        if (matriz[0][0]=='X' && matriz[1][1]=='X' && matriz[2][2]=='X'){
+            msj = "¡Enhorabuena! ¡Has ganado!";
+        }
+        if (matriz[0][2]=='0' && matriz[1][1]=='0' && matriz[2][0]=='0'){
+            msj = "¡Has Perdido!";
+        }
+        if (matriz[0][2]=='X' && matriz[1][1]=='X' && matriz[2][0]=='X'){
+            msj = "¡Enhorabuena! ¡Has ganado!";
+        }
+
+        // Comprobar columnas
+for (c = 0; c < 3; c++) { // Iterar por columnas
+    for (int i = 0; i < 3; i++) { // Iterar por filas
+        if (matriz[i][c] == '0') {
+            contFilaMaq++;
+        }
+        if (matriz[i][c] == 'X') {
+            contFilaJug++;
+        }
+    }
+    if (contFilaMaq == 3) {
+        return "¡Has Perdido!";
+    }
+    if (contFilaJug == 3) {
+        return "¡Enhorabuena! ¡Has ganado!";
+    }
+    contFilaMaq = 0;
+    contFilaJug = 0; // Reiniciar contadores para la próxima columna
+}
+
+        // En este comprobamos si está vacio o las filas
+        for (int i = 0; i < matriz.length; i++) {
+
+            for (int j = 0; j < matriz[i].length; j++) {
+                // Esto para empate.
+                if (matriz[i][j] == ' ') {
                     contador++;
                 }
-                if(matriz[i][j])
-
-
+                // En estas dos comprobamos las filas si alguien ha ganado
+                if (matriz[i][j] == '0') {
+                    contFilaMaq++;
+                }
+                if (matriz[i][j] == 'X') {
+                    contFilaJug++;
+                }
             }
+            if (contFilaMaq == 3) {
+                msj = "¡Has Perdido!";
+            }
+            if (contFilaJug == 3) {
+                msj = "¡Enhorabuena! ¡Has ganado!";
+            }
+            contFilaMaq = 0;
+            contFilaJug = 0;
 
         }
-        //Si no ha ganado nadie y no hay espacio pues empate
-        if(contador==0 && msj.equals("")){
+        // Si no ha ganado nadie y no hay espacio pues empate
+        if (contador == 0 && msj.equals("")) {
             msj = "¡EMPATE!";
         }
 
@@ -119,5 +185,3 @@ public class Ejercicio32TresEnRaya {
     }
 
 }
-
-    
---
+++ tema7arrays/Navidad.java
@@ -0,0 +1,214 @@
+package tema7arrays;
+
+public class Navidad {
+    public class VectorLib {
+        public static int[] rellenar(int[] vector, int min, int max){
+    
+            int num;
+    
+            for(int i=0; i<vector.length; i++){
+                num = (int) (Math.random() * (max-min+1) + min);
+                vector[i] = num;
+            }
+    
+            return vector;
+        }
+    
+        public static void mostrarVector(int[] vector){
+            for(int i=0; i<vector.length; i++){
+                if(i!=vector.length-1){
+                    System.out.print(vector[i] + ", ");
+                } else {
+                    System.out.println(vector[i]);
+                }
+                
+            }
+        }
+    
+        public static long sumaAcumulativa(int[] vector){
+            int suma = 0;
+    
+            for(int i=0; i<vector.length; i++){
+                suma += vector[i];
+            }
+    
+            return suma;
+    
+        }
+    
+        public static int[] sumarVectores(int[] a, int[] b){
+            int[] result;
+            result = new int[a.length];
+    
+            rellenar(result, 0, 0);
+    
+            for(int i=0; i<a.length; i++){
+                result[i] = a[i] + b[i];
+            }
+    
+            return result;
+        } 
+    
+        public static int[] restarVectores(int[] a, int[] b){
+            int[] result;
+            result = new int[a.length];
+    
+            rellenar(result, 0, 0);
+    
+            for(int i=0; i<a.length; i++){
+                result[i] = a[i] - b[i];
+            }
+    
+            return result;
+        } 
+    
+        public static int[] productoEscalar(int[] vector, int producto){
+    
+            for(int i=0; i<vector.length; i++){
+                vector[i] = vector[i] * producto;
+            }
+    
+            return vector;
+        }
+    
+        public static boolean buscarElemento(int[] vector, int numero) {
+            boolean result = false;
+    
+            for(int i=0; i<vector.length; i++){
+                if(vector[i]==numero){
+                    result = true;
+                }
+            }
+    
+            return result;
+        }
+    
+        public static int calcularFrecuencia(int[] vector, int numero){
+            int result = 0;
+            
+            for(int i=0; i<vector.length; i++){
+                if(vector[i]==numero){
+                    result++;
+                }
+            }
+    
+            return result;
+        } 
+    
+        public static int maximo(int[] vector){
+            int result = 0;
+    
+            for(int i=0; i<vector.length; i++){
+                if(vector[i]>result){
+                    result = vector[i];
+                }
+            }
+    
+            return result;
+        }
+    
+        public static int minimo(int[] vector){
+            int result = vector[0];
+    
+            for(int i=0; i<vector.length; i++){
+                if(vector[i]<result){
+                    result = vector[i];
+                }
+            }
+    
+            return result;
+        }
+    
+        public static int media(int[] vector){
+            int result = 0;
+    
+            for(int i=0; i<vector.length; i++){
+                result += vector[i];
+            }
+    
+            return result/vector.length;
+        }
+    
+        public static int[] invertir(int[] vector){
+            int[] aux;
+            aux = new int[vector.length];
+    
+            for(int i=0; i<vector.length; i++){
+                aux[vector.length-1-i] = vector[i];
+            }
+    
+            return aux;
+        }
+    
+        public static boolean comparar(int[] a, int[] b){
+            boolean result = true;
+    
+            for(int i=0; i<a.length; i++){
+                if(a[i]!=b[i]){
+                    result = false;
+                }
+            }
+    
+    
+            return result;
+        }
+    
+        public static int[] eliminarDuplicados(int[] vector){
+            int cont = 0;
+            for(int i=0; i<vector.length; i++){
+                for(int j=0; j<vector.length; j++){
+                    if(vector[i]==vector[j]){
+                        cont++;
+                        if(cont>1){
+                            vector[j]=0;
+                        }
+                    }
+                }
+                cont = 0;
+            }
+            return vector;
+        }
+    
+        public static int[] bubbleSort(int[] vector, boolean debug){
+            int aux;
+            boolean leave = false;
+    
+            while(!leave){
+                leave = true;
+                for(int i=0; i<vector.length-1; i++){
+                    if(vector[i]>vector[i+1]){
+                        aux = vector[i];
+                        vector[i] = vector[i+1];
+                        vector[i+1] = aux;
+                        leave = false;
+                    } 
+                }
+    
+                if(debug){
+                    mostrarVector(vector);
+                }
+            }
+    
+            return vector;
+        }
+    
+        public static int[] selectionSort(int[] vector, boolean debug){
+    
+            int aux;
+            boolean leave = false;
+    
+            while(!leave){
+                leave = true;
+    
+                
+    
+                if(debug){
+                    mostrarVector(vector);
+                }
+            }
+    
+            return vector;
+        }
+    }
+    
+}
