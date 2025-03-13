82d77d71e14e15a6e4d38dd77d1b5242898a3717
DDDBBBPPP <david.bellon.payer@gmail.com>
2025-03-13 10:05:30 +0100

    sdreggret

--- libreria/src/App.java
+++ libreria/src/App.java
@@ -16,44 +16,87 @@ public class App {
     // 3306 puerto donde escucha el servicio mysql
     // ejemplo nombre de la base de datos.
     private static final String URL = "jdbc:mysql://localhost:3306/ejemplo";
+    private static Connection conexion;
 
     public static void main(String[] args) throws Exception {
+        int opcion = 0;
+        boolean terminado = false;
         try {
-
-            Connection conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+            
+             conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
             if (conexion != null) {
-                //MENSAJE DE CONEXION
+                /* // MENSAJE DE CONEXION
                 System.out.println("Conexion realizada con éxito");
 
-                
-                
-                //Lanzar la consulta
-                
-                //1.crear sentencia sqll
+                // Lanzar la consulta
+
+                // 1.crear sentencia sqll
                 String sql = "SELECT * FROM libro ORDER BY paginas;";
                 Statement consulta = conexion.createStatement();
-                ResultSet resultado =  consulta.executeQuery(sql);
-                //Podemos recorrerlo.
-                //Asi se recorre con el metodo next.
-                while(resultado.next()){
-                    System.out.printf("Titulo %s, Autor %s, Numero de Páginas %d\n",resultado.getString("titulo"),
-                                    resultado.getString("autor"),resultado.getInt("paginas"));
+                ResultSet resultado = consulta.executeQuery(sql);
+                // Podemos recorrerlo.
+                // Asi se recorre con el metodo next.
+                while (resultado.next()) {// Da true / false mientras haya registros
+                    System.out.printf("Titulo %s, Autor %s, Numero de Páginas %d\n", resultado.getString("titulo"),
+                            resultado.getString("autor"), resultado.getInt("paginas"));
                     System.out.println("----------------------------------------------------------------------------");
-                                                            
-                }
 
+                } */
 
-                //cERRAMOS LA CONEXION
+                do {
+                    menu();
+                    System.out.println("Opcion?");
+                    opcion = Integer.parseInt(System.console().readLine());
+        
+                    switch (opcion) {
+                        case 0:
+                            terminado = true;
+                            break;
+                        case 1:
+                            insertarLibro();
+                            break;
+                            case 2:
+                            borrarLibro();
+                            break;
+                            case 3:
+                            editarLibro();
+                            break;
+                        case 4:
+                            listarLibro();
+                            break;
+        
+                        default:
+                            break;
+                    }
+        
+                } while (!terminado);
+
+                // cERRAMOS LA CONEXION
                 conexion.close();
             } else {
                 System.out.println("No se ha podido conectar.");
             }
 
-
         } catch (SQLException sqle) {
             System.out.println("Se ha producido un error con la base de datos." + sqle.getMessage());
 
         }
+
+    }
+
+    public static void menu() {
+        System.out.println("0. Salir");
+        System.out.println("1. Insertar libro");
+        System.out.println("2. Borrar libro");
+        System.out.println("3. Editar libro");// Para el final.
+        System.out.println("4. Listar libros.");
+
+    }
+    /**
+     * Solicita Información sobre un libro. y pulsa enter.
+     * Inserta y devuelve true o no inserta y devuelve false.
+     */
+    private static boolean insertarLibro(){
 
     }
 }
